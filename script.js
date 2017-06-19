9e05dbc4815c480ab5e80f1e5d31c5f1adea448c
numtinum07 <numtinum@rocketmail.com>
2017-06-18 23:37:14 -0400

    last change

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function process()
+
+    function process()
 {
     console.log("Input Changed");
+    var swapRegex = /\b(\w+)\b(.*\n*.*)\b\1\b/i ;
     var text=document.getElementById("myTextArea").value;
-    document.getElementById("myTextArea").value = text+"?";
+    if('$1' != "ok"){
+    document.getElementById("myTextArea").value = text.replace(swapRegex,'$1$2');
+	}
 }
+
+
+
+
