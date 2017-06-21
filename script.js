8b92aac523226e7f30ab72e3fca3d1575532f72e
numtinum07 <numtinum@rocketmail.com>
2017-06-20 23:03:18 -0400

    change fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function process()
 {
     console.log("Input Changed");
-    var swapRegex = /(?!\b(the|is|at|which|and|on|at|a|of|in|at|this|that|or)\b(?:.*\n*.*)\b\1\b)\b(\w+)\b(.*\n*.*)\b\2\b/i ;
+    var swapRegex = /(?!\b(the|is|at|which|and|on|at|a|of|in|at|this|that|or)\b(?:.*\n*.*)\b\1\b)\b(\w+)\b(.*\n*.*)\b\2\b/i;
     var text=document.getElementById("myTextArea").value;
     document.getElementById("myTextArea").value = text.replace(swapRegex,'$2$3');
 }
